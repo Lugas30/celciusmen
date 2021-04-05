c12cec8617dd781704fd962622ee05908861fd05
Lugas Gilas Angkoro <lugasgilas@gmail.com>
2021-04-06 01:52:32 +0700

    Baru dropdown

---
+++ assets/js/side-cart.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+   
